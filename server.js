1e050b935d9278bfdafa7812d02f977dea5e3b45
VemulaDhanush <Vemuladhanush300907@gmail.com>
2026-05-13 11:20:13 +0530

    updated cors

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: "https://krishna-painting.vercel.app",
+  origin: "*", 
   methods: ["GET", "POST"],
   credentials: true
 }));
+
 app.use(express.json());
 
 app.use("/api/enquiry", enquiryRoutes);
