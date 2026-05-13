222c9eea3d5583bd9f1106ce5c0ee3d391ea4bb5
VemulaDhanush <Vemuladhanush300907@gmail.com>
2026-05-13 21:12:50 +0530

    simplified backend

--- server.js
+++ server.js
@@ -1,24 +1,66 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
-const enquiryRoutes = require("./routes/enquiryRoutes");
+const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-
-// connect DB
-connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/enquiry", enquiryRoutes);
+mongoose.connect(
+  "mongodb+srv://Vemula:Dhanush%402006@contractordb.ikzzx8m.mongodb.net/contractorDB"
+)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log(err));
+
+const enquirySchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  location: String,
+  service: String,
+  message: String,
+});
+
+const Enquiry = mongoose.model("Enquiry", enquirySchema);
+
+// POST enquiry
+app.post("/api/enquiry", async (req, res) => {
+  try {
+
+    const newEnquiry = new Enquiry(req.body);
+
+    await newEnquiry.save();
+
+    res.status(201).json({
+      message: "Enquiry saved successfully"
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      message: "Error saving enquiry"
+    });
+
+  }
+});
+
+// GET enquiries
+app.get("/api/enquiry", async (req, res) => {
+
+  const enquiries = await Enquiry.find();
+
+  res.json(enquiries);
+
+});
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
