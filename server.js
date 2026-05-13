2381fb04e0d1c5c9bf715c56d9695419cfb5666c
VemulaDhanush <Vemuladhanush300907@gmail.com>
2026-05-13 16:49:18 +0530

    simple cors fix

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ const PORT = process.env.PORT || 5000;
 // connect DB
 connectDB();
 
-app.use(cors({
-  origin: "https://krishna-painting.vercel.app",
-  methods: ["GET", "POST"],
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/enquiry", enquiryRoutes);
