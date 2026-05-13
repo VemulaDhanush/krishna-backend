fe2f6860bc3ab55d1fb09fb040918460ca3196fa
VemulaDhanush <Vemuladhanush300907@gmail.com>
2026-05-14 05:08:09 +0530

    final cors production fix

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(
